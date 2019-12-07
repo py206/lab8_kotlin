f8c4f01043287443cba9383992ce72ae1c5fb28f
py206 <penny880206@gmail.com>
2019-12-07 21:38:46 +0800

    first commit

---
+++ app/src/main/java/com/example/lab8_kotlin/Main2Activity.kt
@@ -0,0 +1,13 @@
+package com.example.lab8_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
